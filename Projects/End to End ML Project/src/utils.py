2740ba258d7a47c05c54baf8210e3bbb82c08836
Pranav S P <psp123.2003@gmail.com>
2025-10-08 11:40:00 +0530

    End to End ML Project files upload.

---
+++ Projects/End to End ML Project/src/utils.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import dill
+from sklearn.metrics import r2_score
+
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'wb') as file:
+            dill.dump(obj, file)
+    except Exception as e:
+        raise CustomException("Error occurred while saving object", e)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        for model_name, model in models.items():
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            score = r2_score(y_test, y_pred)
+            report[model_name] = score
+        return report
+    except Exception as e:
+        raise CustomException("Error occurred while evaluating models", e)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file:
+            obj = dill.load(file)
+        return obj
+    except Exception as e:
+        raise CustomException("Error occurred while loading object", e)
